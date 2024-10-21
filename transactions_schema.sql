e08d483af8a9df3dd411952f27b2856eccaea84a
dubesoftware <therealdubesoftware@gmail.com>
2024-10-21 16:13:15 +0200

    Add Data Definition Language for customers table

---
+++ transactions_schema.sql
@@ -0,0 +1,11 @@
+-- The Schema contained herein is designed to track transactions between customers and merchants. Therefore, we have three tables mapping to the thrtee entities/ objects that we have identified. These are Customers, Merchants and Transactions.
+
+-- We define the relationship between customers and merchants via the Transactions table.
+
+-- DDL for Customers table
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    phone_number VARCHAR(15)
+);
