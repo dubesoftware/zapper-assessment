55c6e51c9436e118d55ea8c9adaf1a79000ccd13
dubesoftware <therealdubesoftware@gmail.com>
2024-10-21 16:15:02 +0200

    Add DDL for mechants table

--- transactions_schema.sql
+++ transactions_schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE customers (
     email VARCHAR(255) UNIQUE NOT NULL,
     phone_number VARCHAR(15)
 );
+
+-- DDL for merchants table
+CREATE TABLE merchants (
+    merchant_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(255)
+);
