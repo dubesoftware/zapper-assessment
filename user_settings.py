43d20048c64496559b94cfc4eeacd80519e488e4
dubesoftware <therealdubesoftware@gmail.com>
2024-10-21 16:41:03 +0200

    Ass test cases for `is_feature_enabled` function

--- user_settings.py
+++ user_settings.py
@@ -2,3 +2,8 @@
 def is_feature_enabled(settings: str, setting_index: int) -> bool:
     index = setting_index - 1
     return settings[index] == '1'
+
+# Test cases for `is_feature_enabled` function
+print(is_feature_enabled('00000000', 7))  # Output: False
+print(is_feature_enabled('00000010', 7))  # Output: True
+print(is_feature_enabled('11111111', 4))  # Output: True
